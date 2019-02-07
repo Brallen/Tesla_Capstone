d7fe665d523ebc8f883203550ce79710274d1902
Brett Case <brett.case97@gmail.com>
2019-02-06 18:24:44 -0800

    changed server to serve static page

--- app.js
+++ app.js
@@ -1,28 +1,20 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var burtonRouter = require('./routes/burton'); //more goofing around
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+let app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Assets')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/Burton', burtonRouter); //testing out new page
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
