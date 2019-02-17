2db2e99a5b66c1b24a064f99d49cbd1444f1dc8d
Brett Case <brett.case97@gmail.com>
2019-02-17 14:40:50 -0800

    adding finished markup

--- Assets/Scripts/main.js
+++ Assets/Scripts/main.js
@@ -1,13 +1,43 @@
 window.onload = function(){
     let controlModal = document.getElementsByClassName('container--modal_controls')[0];
-    let controlModalOpen = document.getElementById('modal--control_open');
-    let controlModalClose = document.getElementById('modal--control_close');
+    let mediaModal = document.getElementsByClassName('container--modal_media')[0];
+    let climateModal = document.getElementsByClassName('container--modal_climate')[0];
+    let chargingModal = document.getElementsByClassName('container--modal_charging')[0];
+    let summonModal = document.getElementsByClassName('container--modal_summon')[0];
 
-    controlModalClose.onclick = function() {
+    document.getElementById('modal--control_open').onclick = function() {
+        controlModal.style.display = 'block';
+    };
+    document.getElementById('modal--control_close').onclick = function() {
         controlModal.style.display = 'none';
     };
 
-    controlModalOpen.onclick = function() {
-        controlModal.style.display = 'block';
+    document.getElementById('modal--media_open').onclick = function() {
+        mediaModal.style.display = 'block';
     };
+    document.getElementById('modal--media_close').onclick = function() {
+        mediaModal.style.display = 'none';
+    };
+
+    document.getElementById('modal--climate_open').onclick = function() {
+        climateModal.style.display = 'block';
+    };
+    document.getElementById('modal--climate_close').onclick = function() {
+        climateModal.style.display = 'none';
+    };
+
+    document.getElementById('modal--charging_open').onclick = function() {
+        chargingModal.style.display = 'block';
+    };
+    document.getElementById('modal--charging_close').onclick = function() {
+        chargingModal.style.display = 'none';
+    };
+
+    document.getElementById('modal--summon_open').onclick = function() {
+        summonModal.style.display = 'block';
+    };
+    document.getElementById('modal--summon_close').onclick = function() {
+        summonModal.style.display = 'none';
+    };
+
 }
