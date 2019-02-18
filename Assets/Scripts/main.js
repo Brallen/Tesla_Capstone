fb3302daa8b79a94777d374a551ff436f55d8061
Brett Case <brett.case97@gmail.com>
2019-02-18 14:48:41 -0800

    cleaning up front end js

--- Assets/Scripts/main.js
+++ Assets/Scripts/main.js
@@ -1,7 +1,6 @@
 window.onload = function(){
+    // Modal code
     let controlModal = document.getElementsByClassName('container--modal_controls')[0];
-    let controlModalOpen = document.getElementById('modal--control_open');
-    let controlModalClose = document.getElementById('modal--control_close');
     let mediaModal = document.getElementsByClassName('container--modal_media')[0];
     let climateModal = document.getElementsByClassName('container--modal_climate')[0];
     let chargingModal = document.getElementsByClassName('container--modal_charging')[0];
@@ -9,41 +8,54 @@ window.onload = function(){
 	let flashbutton = document.getElementById('flashlights_btn');
     let trunkbutton = document.getElementById('opentrunk_btn');
     let frunkbutton = document.getElementById('openfrunk_btn');
-	
-	var isLocked = 0;
-	
-	if (isLocked == 0) document.getElementById('lock').innerHTML = "Lock";
-	else document.getElementById('lock').innerHTML = "Unlock";
-
+    
     document.getElementById('modal--control_open').onclick = function() {
         controlModal.style.display = 'block';
     };
     document.getElementById('modal--control_close').onclick = function() {
         controlModal.style.display = 'none';
     };
-
+    
     document.getElementById('modal--media_open').onclick = function() {
         mediaModal.style.display = 'block';
     };
     document.getElementById('modal--media_close').onclick = function() {
         mediaModal.style.display = 'none';
     };
-
     document.getElementById('modal--climate_open').onclick = function() {
         climateModal.style.display = 'block';
     };
     document.getElementById('modal--climate_close').onclick = function() {
         climateModal.style.display = 'none';
     };
-	
+    document.getElementById('modal--charging_open').onclick = function() {
+        chargingModal.style.display = 'block';
+    };
+    document.getElementById('modal--charging_close').onclick = function() {
+        chargingModal.style.display = 'none';
+    };
+    
+    document.getElementById('modal--summon_open').onclick = function() {
+        summonModal.style.display = 'block';
+    };
+    document.getElementById('modal--summon_close').onclick = function() {
+        summonModal.style.display = 'none';
+    };
+    
+    // Async requests
+    var isLocked = 0;
+    
+    if (isLocked == 0) document.getElementById('lock').innerHTML = "Lock";
+    else document.getElementById('lock').innerHTML = "Unlock";
+
 	lock.onclick = function() {
-		if (isLocked == 0) {
-			$.ajax({
-				url:"lock"
-				}).done(function(response){
-					//alert(response);
-					isLocked = 1;
-					document.getElementById('lock').innerHTML = "Unlock";
+        if (isLocked == 0) {
+            $.ajax({
+                url:"lock"
+            }).done(function(response){
+                //alert(response);
+                isLocked = 1;
+                document.getElementById('lock').innerHTML = "Unlock";
 				});
 		}
 		else {
@@ -64,20 +76,6 @@ window.onload = function(){
 				//alert(response);
 			});
 	}
-
-    document.getElementById('modal--charging_open').onclick = function() {
-        chargingModal.style.display = 'block';
-    };
-    document.getElementById('modal--charging_close').onclick = function() {
-        chargingModal.style.display = 'none';
-    };
-
-    document.getElementById('modal--summon_open').onclick = function() {
-        summonModal.style.display = 'block';
-    };
-    document.getElementById('modal--summon_close').onclick = function() {
-        summonModal.style.display = 'none';
-    };
 
     flashbutton.onclick = function(){
       $.ajax({
