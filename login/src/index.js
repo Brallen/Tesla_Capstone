96c39c888cb5a5095865d755dc868458bf1fc74b
jazeng <zengja@oregonstate.edu>
2019-02-22 14:09:10 -0800

    Cleaned up login folder

--- login/src/index.js
+++ login/src/index.js
@@ -1,12 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+  state = {
+   response: '',
+   password: '',
+   responseToPost: '',
+   username: '',
+ };
+ componentDidMount() {
+   this.callApi()
+     .then(res => this.setState({ response: res.express }))
+     .catch(err => console.log(err));
+ }
+ callApi = async () => {
+   const response = await fetch('/api/hello');
+   const body = await response.json();
+   if (response.status !== 200) throw Error(body.message);
+   return body;
+ };
+ handleSubmit = async e => {
+   e.preventDefault();
+   const response = await fetch('/api/world', {
+     method: 'POST',
+     headers: {
+       'Content-Type': 'application/json',
+     },
+     body: JSON.stringify(this.state),
+     //body: JSON.stringify({ password: this.state.password }),
+   });
+   const body = await response.text();
+   this.setState({ responseToPost: body });
+ };
+  render() {
+    return (
+      <div className="App">
+        <p>{this.state.response}</p>
+       <form onSubmit={this.handleSubmit}>
+         <p>
+           <strong>Login:</strong>
+         </p>
+         Username: <input name = "username" type="text" value={this.state.username} onChange={e => this.setState({ username: e.target.value })} />
+         Password: <input name = "password" type ="password" value={this.state.password} onChange={e => this.setState({ password: e.target.value })}/>
+         <button type="submit">Submit</button>
+       </form>
+       <p>{this.state.responseToPost}</p>
+      </div>
+    );
+  }
+}
+ReactDOM.render(<App/>, document.getElementById('root'));
