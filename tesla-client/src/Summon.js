9b1607814ba2d3e362e9e56d9a0b06ce0d2c33a0
Burton Jaursch <burton.jaursch@gmail.com>
2019-04-12 13:22:13 -0700

    Adding basic SummonModal component for now. Not connected to Main.js

---
+++ tesla-client/src/Summon.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {store} from './store/index.js';
+import { connect } from 'react-redux';
+import { axios } from 'axios';
+
+class SummonModal extends Component{
+
+  //The Modal - need to include in Main.js for link
+  render(){
+    return(
+
+    );
+  }
+}
